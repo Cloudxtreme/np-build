697b2ed0b92bc2dcc413a932e0bd4eee9d2dbe7a
Marco Bonomo <marco.bonomo@gmail.com>
2014-11-11 14:38:03 +0100

    first version almost working

--- np-build.js
+++ np-build.js
@@ -4,24 +4,57 @@
  * Module dependencies.
  */
 
-var program = require('commander');
-program
-  .version('0.0.1')
-  .option('-u, --user [type]', 'crea un utente [admin]', 'user')
-  .option('-n, --name [type]', 'crea un dominio', 'domainname');
+ var program = require('commander');
+ var nconf = require('nconf');
+ var siteconf = require('nconf');
+ var os = require("os");
+ var fs = require('fs');
+ var generatePassword = require('password-generator');
+ var sys = require('sys')
+ var exec = require('child_process').exec;
+ var child;
+ var network = require('network');
+ var myip = network.get_public_ip(function(err, ip) {
+ })
 
-if (process.argv.length < 3) {
-	program.help();
+ program
+ .version('0.0.1')
+ .option('-u, --user [string]', 'crea un utente', 'admin')
+ .option('-n, --domainname [string]', 'crea un dominio', os.hostname());
+
+ if (process.argv.length < 3) {
+  program.help();
 }
 
+if (!(fs.existsSync('./config.json'))) {
+ 
+   console.log('non hai un file di configurazione, lo creo...');
+    nconf.use('file', { file: './config.json' });
+    nconf.load();
+    nconf.set('ip', +myip);
+    nconf.set('hostname', os.hostname());
+    nconf.set('email', '');
+    nconf.set('wp_user', '');
+    nconf.save(function (err) {
+      if (err) {
+        console.error(err.message);
+        return;
+      }
+      console.log('Configuration saved successfully.');
+    });
+  } 
+  else {
+    nconf.use('file', { file: './config.json' });
+  nconf.load();
+    
 program.parse(process.argv);
 var username = program.user;
 var domainname = program.domainname; 
-console.log(username+domainname);
 
-var menu = require('terminal-menu')({ width: 75, x: 0, y: 0 });
+
+var menu = require('terminal-menu')({ width: process.stdout.columns - 4, x: 0, y: 0 });
 menu.reset();
-menu.write('np-build is a command line tool for creating vesta control panel hosting\n');
+menu.write('np-build is a tool for vesta cp\n');
 menu.write('-------------------------\n');
 
 menu.add('1 . Crea hosting e database');
@@ -33,38 +66,92 @@ menu.on('select', function (label) {
     menu.close();
     console.log('SELECTED: ' + label);
     var scelta = label.substring(0,1);
-
+    siteconf.use('file', { file: username+'.json', dir:'config_web/', search:true });
+    siteconf.load();
    	switch(scelta) {
    	    case '1':
-   	    	creahosting();
-   	    	creadb();  	
+          creautente(username);
+   	    	creahosting(username, domainname);
+   	    	creadb(username);
+          salvaConfig();  	
    	        break;
    	    case '2':
-   	    	creahosting();
-   	    	creadb();
+          creautente(username);
+          creahosting(username, domainname);
+          creadb(username);
    	    	installwp();
+          salvaConfig(); 
    	        break;
    	    case '3':
-   	    	creahosting();
+          creautente(username);
+          creahosting(username, domainname);
+          salvaConfig(); 
    	    	break;
    	}
     
 
 });
+
 menu.createStream().pipe(process.stdout);
 
-var generatePassword = require('password-generator');
+}
+
+function salvaConfig() {
+  siteconf.save(function (err) {
+      if (err) {
+        console.error(err.message);
+        return;
+      }
+      console.log('operazione completata');
+    });
+}
+
+function creautente (user) {
+  var password = generatePassword(12,false);
+    console.log('creo l\'utente'+ user +' su vesta con password' + password + '...');
+    console.log ('/usr/local/vesta/bin/v-add-user ' + user + ' ' + password + ' ' + nconf.get('email'));
+    //exec('/usr/local/vesta/bin/v-add-user ' + user + ' ' + password + ' ' +nconf.get('email'), puts);
+    
+    siteconf.set('vesta:username', user);
+    siteconf.set('vesta:password', password);
+    
+} 
 
 function creahosting(user, domain) {
-	var password = generatePassword(12,false);
-	console.log('crea hosting');
-	console.log(password);
+    console.log('creazione hosting ' + domainname + ' con ip ' +nconf.get('ip') );
+    console.log ('/usr/local/vesta/bin/v-add-web-domain ' + user + ' ' + domainname + ' ' + nconf.get('ip'));
+    //exec('/usr/local/vesta/bin/v-add-web-domain ' + user + ' ' + domainname + ' ' +nconf.get('ip'), puts);
 }
 
 function creadb (user) {
-	console.log('crea db');
+ console.log('crea db');
+ var password_db = generatePassword(12,false);
+ console.log ('/usr/local/vesta/bin/v-add-database ' + user + ' DB U ' + password_db);
+ //exec('/usr/local/vesta/bin/v-add-database ' + user + ' DB U ' + password, puts);
+  siteconf.set('database:db', user+'_DB');
+  siteconf.set('database:username', user+'_U');
+    siteconf.set('database:password', password_db); 
 }
 
-function installwp (user) {
+function installwp (user,domain) {
 	console.log('installa wp');
+  var password_wp = generatePassword(12,false);
+  dir='/home/'+user+'/web/'+domain+'/public_html';
+  exec('cd '+dir);
+  exec('usermod -a -G '+user+' admin');
+  exec('chmod -R 777 '+dir);
+  exec('sudo -u admin wp core download --locale=it_IT');
+  exec('sudo -u admin wp core config --dbname='+user+'_DB --dbuser='+user'_U --dbpass='+password_wp+' --locale=it_IT');
+  exec('sudo -u admin wp core install --url=www.'+domain+' --title='+domain' --admin_user='+nconf.get('wp_user')+' --admin_password='+password_wp+' --admin_email='+nconf.get('email');
+  exec('sudo -u admin wp rewrite structure /%postname%/');
+  exec('sudo -u admin wp plugin install wordpress-seo --activate');
+  exec('rm /index.html');
+  exec('chmod -R 755 '+dir);
+  exec('chown -R '+user+' '+dir;
+
+  siteconf.set('wordpress:username', nconf.get('wp_user'));
+    siteconf.set('database:password', password_wp); 
+    
 }
+
+function puts(error, stdout, stderr) { sys.puts(stdout) }

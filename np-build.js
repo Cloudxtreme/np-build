6913db2a7abd3101bbb61efc270ac252f195b6a5
Marco Bonomo <marco.bonomo@gmail.com>
2014-11-12 17:21:07 +0100

    proviamo

--- np-build.js
+++ np-build.js
@@ -10,7 +10,7 @@
  var os = require("os");
  var fs = require('fs');
  var generatePassword = require('password-generator');
- var sys = require('util')
+ var sys = require('sys')
  var exec = require('child_process').exec;
  var child;
  var network = require('network');

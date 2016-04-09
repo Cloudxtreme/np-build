a69305102bc5170f9ff7ac6d1c348bce182f0bb6
Marco Bonomo <marco.bonomo@gmail.com>
2016-04-09 12:34:43 +0200

    switch to native execSync

--- np-build.js
+++ np-build.js
@@ -13,6 +13,7 @@
  var sys = require('util')
  var child_process = require('child_process');
  var child;
+var execSync = require('child_process').execSync;
  var network = require('network');
  var myip = network.get_public_ip(function(err, ip) {
  });
@@ -52,10 +53,12 @@ if (!(fs.existsSync('./config.json'))) {
   var ip = nconf.get('ip');
   var hostname = nconf.get('hostname');
   var wp_user  = nconf.get('wp_user');
+
   console.log(hostname);
 program.parse(process.argv);
 var username = program.user;
 var domainname = program.domainname;
+
 
 
 var menu = require('terminal-menu')({ width: process.stdout.columns - 4, x: 0, y: 0 });
@@ -134,23 +137,23 @@ function mailConfig(username,domain,email) {
 function creautente (user, email) {
   var password = generatePassword(12,false);
     console.log('creo l\'utente'+ user +' su vesta con password ' + password + '...');
-    //console.log ('/usr/local/vesta/bin/v-add-user ' + user + ' ' + password + ' ' +email );
-    execSync('/usr/local/vesta/bin/v-add-user ' + user + ' ' + password + ' ' +email, puts);
+    console.log ('sudo -u root /usr/local/vesta/bin/v-add-user ' + user + ' ' + password + ' ' +email );
+    execSync('sudo -u root /usr/local/vesta/bin/v-add-user ' + user + ' ' + password + ' ' +email, {stdio:[0,1,2]});
     siteconf.set('vesta:username', user);
     siteconf.set('vesta:password', password);
 }
 
 function creahosting(user,domain,ip) {
     console.log('creazione hosting ' + domainname + ' con ip ' + ip);
-    //console.log ('/usr/local/vesta/bin/v-add-web-domain ' + user + ' ' + domainname + ' ' + ip);
-    execSync('/usr/local/vesta/bin/v-add-web-domain ' + user + ' ' + domainname + ' ' +ip, puts);
+    console.log ('sudo -u root /usr/local/vesta/bin/v-add-web-domain ' + user + ' ' + domainname + ' ' + ip);
+    execSync('sudo -u root /usr/local/vesta/bin/v-add-web-domain ' + user + ' ' + domainname + ' ' +ip, puts);
 }
 
 function creadb (user) {
  console.log('crea db');
  var password_db = generatePassword(12,false);
- console.log ('/usr/local/vesta/bin/v-add-database ' + user + ' DB U ' + password_db);
- execSync('/usr/local/vesta/bin/v-add-database ' + user + ' DB U ' + password_db, puts);
+ console.log ('sudo -u root /usr/local/vesta/bin/v-add-database ' + user + ' DB U ' + password_db);
+ execSync('sudo -u root /usr/local/vesta/bin/v-add-database ' + user + ' DB U ' + password_db, puts);
   siteconf.set('database:db', user+'_DB');
   siteconf.set('database:username', user+'_U');
     siteconf.set('database:password', password_db);
@@ -162,15 +165,15 @@ function installwp (user,domain,email,wp_user,password_db) {
   var password_wp = generatePassword(12,false);
   dir='/home/'+user+'/web/'+domain+'/public_html';
   //execSync('cd '+dir);
-  execSync('usermod -a -G '+user+' admin');
-  execSync('chmod -R 777 '+dir);
+  execSync('sudo -u root  usermod -a -G '+user+' admin');
+  execSync('sudo -u root  chmod -R 777 '+dir);
   console.log('scarico wordpress');
-  execSync('sudo -u admin wp core download --locale=it_IT --path='+dir);
+  execSync('sudo -u admin  wp core download --locale=it_IT --path='+dir);
   console.log('scrivo un file di configurazione');
-  console.log('sudo -u admin wp core config --dbname='+user+'_DB --dbuser='+user+'_U --dbpass='+password_db+' --locale=it_IT')
+  console.log('wp core config --dbname='+user+'_DB --dbuser='+user+'_U --dbpass='+password_db+' --locale=it_IT')
   execSync('sudo -u admin wp core config --dbname='+user+'_DB --dbuser='+user+'_U --dbpass='+password_db+' --locale=it_IT --path='+dir);
   console.log('installo wordpress e creo le tabelle nel db');
-  console.log('sudo -u admin wp core install --url=www.'+domain+' --title='+domain+' --admin_user='+wp_user+' --admin_password='+password_wp+' --admin_email='+email+' --path='+dir);
+  console.log('sudo -u admin  wp core install --url=www.'+domain+' --title='+domain+' --admin_user='+wp_user+' --admin_password='+password_wp+' --admin_email='+email+' --path='+dir);
   execSync('sudo -u admin wp core install --url=www.'+domain+' --title='+domain+' --admin_user='+wp_user+' --admin_password='+password_wp+' --admin_email='+email+' --path='+dir);
   console.log('imposto i permalink');
   execSync('sudo -u admin wp rewrite structure /%postname%/ --path='+dir);
@@ -178,31 +181,12 @@ function installwp (user,domain,email,wp_user,password_db) {
   console.log('Wordpress-seo');
   execSync('sudo -u admin wp plugin install wordpress-seo --activate --path='+dir);
   execSync('rm '+dir+'/index.html');
-  execSync('chmod -R 755 '+dir);
-  execSync('chown -R '+user+' '+dir);
+  execSync('sudo -u root  chmod -R 755 '+dir);
+  execSync('sudo -u root  chown -R '+user+' '+dir);
 
   siteconf.set('wordpress:username', wp_user);
     siteconf.set('wordpress:password', password_wp);
 
-}
-
-function execSync(command) {
-// Run the command in a subshell
-child_process.exec(command + ' 2>&1 1>/root/np-build/output && echo done! > /root/np-build/done');
-
-// Block the event loop until the command has executed.
-while (!fs.existsSync('/root/np-build/done')) {
-// Do nothing
-}
-
-// Read the output
-var output = fs.readFileSync('/root/np-build/output');
-
-// Delete temporary files.
-fs.unlinkSync('/root/np-build/output');
-fs.unlinkSync('/root/np-build/done');
-
-return output;
 }
 
 function puts(error,stdout,stderr){sys.puts(stdout); }
